ed020d97119701d0b5fb45f0fe27bcf0af985124
mazen alshehri <mazen4sa@gmail.com>
2025-03-10 00:18:16 +0300

    lab-6

---
+++ script.js
@@ -0,0 +1,42 @@
+let likesCount = 100;
+let dislikesCount = 20;
+
+const likeBtn = document.getElementById("likeBtn");
+const dislikeBtn = document.getElementById("dislikeBtn");
+const commentBox = document.getElementById("commentBox");
+const submitBtn = document.getElementById("submit");
+const clearBtn = document.getElementById("clear");
+const commentsList = document.getElementById("commentsList");
+
+likeBtn.addEventListener("click", () => {
+  likesCount++;
+  likeBtn.innerText = `👍 ${likesCount}`;
+  disableButtons();
+});
+
+dislikeBtn.addEventListener("click", () => {
+  dislikesCount++;
+  dislikeBtn.innerText = `👎 ${dislikesCount}`;
+  disableButtons();
+});
+
+submitBtn.addEventListener("click", () => {
+  const commentText = commentBox.value.trim();
+  if (commentText !== "") {
+    const commentElement = document.createElement("div");
+    commentElement.classList.add("comment-item");
+    commentElement.innerText = commentText;
+    commentsList.appendChild(commentElement);
+    commentBox.value = "";
+  }
+});
+
+clearBtn.addEventListener("click", () => {
+  commentBox.value = "";
+});
+
+function disableButtons() {
+  likeBtn.disabled = true;
+  dislikeBtn.disabled = true;
+  submitBtn.disabled = true;
+}
